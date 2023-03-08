49bd1d82bb75a6499f53e456ce5752377ab1abc4
subin <subinshrestha429@gmail.com>
2023-03-08 11:05:46 +0545

    feat: created template public modules

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./public/public.module').then(mod => mod.PublicModule),
+    component: HomeComponent
   }
 ];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class PublicRoutingModule { }
