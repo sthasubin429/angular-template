ab834c93cf5af0d01f42bcce7983d97cbf78978c
subin <subinshrestha429@gmail.com>
2023-03-13 10:35:11 +0545

    feat: created unless directive

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PipesModule } from '@shared/pipes/pipes.module';
+import { DirectivesModule } from '@shared/directives/directives.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DashboardDetailComponent } from './components/dashboard-detail/dashboard-detail.component';
@@ -17,7 +18,8 @@ import { DashboardShellComponent } from './components/dashboard-shell/dashboard-
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    PipesModule
+    PipesModule,
+    DirectivesModule
   ]
 })
 export class DashboardModule { }
---
+++ src/app/shared/directives/directives.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UnlessDirective } from './unless/unless.directive';
+
+@NgModule({
+  declarations: [
+    UnlessDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    UnlessDirective
+  ]
+})
+export class DirectivesModule { }
