f86fe629fb8685ea6085dc2201e0a6b2106b5394
subin <subinshrestha429@gmail.com>
2023-03-14 14:13:23 +0545

    feat: added state transfer in dashboard component

--- src/app/pages/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { TransferState, makeStateKey, StateKey } from '@angular/platform-browser';
 import { MockDataService } from '@core/http/mock-data/mock-data.service';
 import { mockData } from '@core/interfaces';
 
@@ -12,13 +14,23 @@ export class DashboardComponent implements OnInit {
   public noData: string|null = null;
   public data: mockData[]|null = null;
 
-  constructor(private mock: MockDataService) { }
+  stateKey: StateKey<mockData[]> = makeStateKey<mockData[]>('mock-data-state');
+
+  constructor(private mock: MockDataService, @Inject(PLATFORM_ID) private platformId: object, private transferState: TransferState) { }
 
   ngOnInit(): void {
-    this.mock.getData().subscribe((data: mockData[]) => {
-      console.dir(data);
-      this.data = data;
-    });
+    if (this.transferState.hasKey(this.stateKey)) {
+      const mData: mockData[]|null = this.transferState.get<mockData[] | null>(this.stateKey, null);
+      this.transferState.remove(this.stateKey);
+      this.data = mData;
+    }
+    else {
+      this.mock.getData().subscribe((data: mockData[]) => {
+        console.dir(data);
+        if (isPlatformServer(this.platformId)) {
+          this.transferState.set(this.stateKey, data);
+        }
+      });
+    }
   }
-
 }
