ead4b0bf0e36755373f458cec3d32fc91fff6567
subin <subinshrestha429@gmail.com>
2023-05-10 17:48:39 +0545

    test: unit test for unless directive

--- src/app/shared/directives/unless/unless.directive.spec.ts
+++ src/app/shared/directives/unless/unless.directive.spec.ts
@@ -1,8 +1,46 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 import { UnlessDirective } from './unless.directive';
+@Component({
+  template: `
+    <ng-container *appUnless="condition">
+      <div>UnlessDirective</div>
+    </ng-container>
+  `
+})
+class TestComponent {
+  condition = false;
+}
 
 describe('UnlessDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UnlessDirective();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TestComponent, UnlessDirective ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the template when condition is false', () => {
+    expect(fixture.nativeElement.innerHTML).toContain('UnlessDirective');
+  });
+
+  it('should not display the template when condition is true', () => {
+    component.condition = true;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.innerHTML).not.toContain('UnlessDirective');
   });
 });
+
