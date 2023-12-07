6780302d0e93fd79f20ba9583118622f9189fc66
subin <48222247+sthasubin429@users.noreply.github.com>
2023-12-07 11:26:22 +0545

    fix: test specs

--- src/app/pages/dashboard/components/dashboard-detail/dashboard-detail.component.spec.ts
+++ src/app/pages/dashboard/components/dashboard-detail/dashboard-detail.component.spec.ts
@@ -1,18 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardDetailComponent } from './dashboard-detail.component';
+import { MockDataService } from '@core/http/mock-data/mock-data.service';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PipesModule } from '@shared/pipes/pipes.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('DashboardDetailComponent', () => {
   let component: DashboardDetailComponent;
   let fixture: ComponentFixture<DashboardDetailComponent>;
+  let mock: MockDataService;
+  let route: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardDetailComponent],
+      imports: [DashboardDetailComponent, HttpClientTestingModule, RouterTestingModule, PipesModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardDetailComponent);
     component = fixture.componentInstance;
+    mock = TestBed.inject(MockDataService);
+    route = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
--- src/app/pages/dashboard/components/dashboard/dashboard.component.spec.ts
+++ src/app/pages/dashboard/components/dashboard/dashboard.component.spec.ts
@@ -1,18 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { MockDataService } from '@core/http/mock-data/mock-data.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PipesModule } from '@shared/pipes/pipes.module';
+import { DirectivesModule } from '@shared/directives/directives.module';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let mock: MockDataService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent],
+      imports: [HttpClientTestingModule, PipesModule, DirectivesModule, DashboardComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+    mock = TestBed.inject(MockDataService);
     fixture.detectChanges();
   });
 
